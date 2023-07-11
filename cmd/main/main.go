e6371b6246aeae1505e5c0995f8894c647a10170
Lh <lihao1017@qq.com>
2023-07-11 22:06:34 +0800

    commit all

---
+++ cmd/main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/SiriusLLL/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
---
+++ pkg/routes/bookstore-routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/SiriusLLL/go-bookstore/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+// 建立路由
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookid}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookid}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookid}", controllers.DeleteBook).Methods("DELETE")
+}
